0499987bfb822170b7612aa00529561be0aaf558
BerlinDiary <fxm31415926@gmail.com>
2017-06-05 16:49:34 +0800

    ok

--- src/main/java/FtpUpload.java
+++ src/main/java/FtpUpload.java
@@ -3,9 +3,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by fengxuming on 2017/6/5.
@@ -13,37 +11,59 @@ import java.io.IOException;
 public class FtpUpload {
 
 
-    public boolean upload(File file,String host,String username,String password,String path)
-    {
-
-        FTPClient ftpClient = new FTPClient();
-        FileInputStream fis = null;
-
+    /**
+     * Description: 向FTP服务器上传文件
+     *
+     * @param url      FTP服务器hostname
+     * @param port     FTP服务器端口
+     * @param username FTP登录账号
+     * @param password FTP登录密码
+     * @param path     FTP服务器保存目录
+     * @param filename 上传到FTP服务器上的文件名
+     * @param input    输入流
+     * @return 成功返回true，否则返回false
+     * @Version1.0 Jul 27, 2008 4:31:09 PM by 崔红保（cuihongbao@d-heaven.com）创建
+     */
+    public static boolean uploadFile(String url, int port, String username, String password, String path, String filename, InputStream input) {
+        boolean success = false;
+        FTPClient ftp = new FTPClient();
         try {
-            ftpClient.connect(host);
-            ftpClient.login(username, password);
-            fis = new FileInputStream(file);
-            //设置上传目录
-            ftpClient.changeWorkingDirectory(path);
-            ftpClient.setBufferSize(1024);
-            ftpClient.setControlEncoding("UTF8");
-            //设置文件类型（二进制）
-            ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE);
-            ftpClient.storeFile(path, fis);
+            int reply;
+            ftp.connect(url, port);//连接FTP服务器
+            //如果采用默认端口，可以使用ftp.connect(url)的方式直接连接FTP服务器
+            ftp.login(username, password);//登录
+            reply = ftp.getReplyCode();
+            if (!FTPReply.isPositiveCompletion(reply)) {
+                ftp.disconnect();
+                return success;
+            }
+            ftp.changeWorkingDirectory(path);
+            ftp.storeFile(filename, input);
+
+            input.close();
+            ftp.logout();
+            success = true;
         } catch (IOException e) {
             e.printStackTrace();
-            throw new RuntimeException("FTP客户端出错！", e);
         } finally {
-            IOUtils.closeQuietly(fis);
-            try {
-                ftpClient.disconnect();
-            } catch (IOException e) {
-                e.printStackTrace();
-                throw new RuntimeException("关闭FTP连接发生异常！", e);
+            if (ftp.isConnected()) {
+                try {
+                    ftp.disconnect();
+                } catch (IOException ioe) {
+                }
             }
         }
-
-
+        return success;
     }
 
+//    public void testUpLoadFromDisk(){
+//        try {
+//            FileInputStream in=new FileInputStream(new File("D:/test.txt"));
+//            boolean flag = uploadFile("127.0.0.1", 21, "test", "test", "D:/ftp", "test.txt", in);
+//            System.out.println(flag);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//    }
+// 上传
 }
